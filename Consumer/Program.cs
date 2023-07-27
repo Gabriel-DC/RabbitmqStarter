66a4e4c3760c521a5ab8b967a28fcedfbe9988cf
Gabriel Almeida <57327840+Gabriel-DC@users.noreply.github.com>
2023-07-27 00:49:26 -0300

    Publisher & Consumer

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Consumer
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Consumer
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConnectionFactory factory = new()
+            {
+                HostName = "localhost"
+            };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "my_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += OnReceiveMessage;
+
+            channel.BasicConsume(
+                queue: "my_queue",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.ReadKey();
+        }
+
+        public static void OnReceiveMessage(object? sender, BasicDeliverEventArgs ea)
+        {
+            byte[] body = ea.Body.ToArray();
+            string message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Recebida: {message}");
         }
     }
 }
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Publisher
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "my_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            string message = "Teste envio de mensagem";
+
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "my_queue",
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine($" [x] Enviada: {message}");
+
+            Console.ReadKey();
         }
     }
 }
